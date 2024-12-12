20b4f34dc88e069b2af2f77c8814a15ac24df06b
Asset <a11@MacBook-Air.local>
2024-12-12 18:17:03 +0500

    new

---
+++ StocksApp/NetworkingManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkingManager.swift
+//  StocksApp
+//
+//  Created by Asset on 12/11/24.
+//
+
+import Foundation
---
+++ StocksApp/StockLogoData.swift
@@ -0,0 +1,8 @@
+//
+//  StockLogoData.swift
+//  StocksApp
+//
+//  Created by Asset on 12/12/24.
+//
+
+import Foundation
